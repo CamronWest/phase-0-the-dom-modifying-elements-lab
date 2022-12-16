9e5f9464f5515337fbfbb602e96949f7c63cc019
Camron <Cawe219@gmail.com>
2022-12-15 22:51:46 -0600

    commit tree bd8f439c3d2a40c15e82c3b3537bbedd9408a2bf

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+    main.remove();
+
+const newHeader = document.createElement('h1');
+// const body = document.getElementById(body);
+    newHeader.id = 'victory';
+    newHeader.textContent = 'Camron is the champion';
+    document.body.append(newHeader);
+
+
